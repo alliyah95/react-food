2da35c5a1ef4a2559ad2d0a52f02ce83eab398cd
alliyah95 <74038500+alliyah95@users.noreply.github.com>
2022-12-21 19:30:57 +0800

    Fix `Nav` location

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import Nav from "./components/Nav/Nav";
+import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Main from "./components/Main";
-import Products from "./components/Products/Products";
 import Footer from "./components/Footer";
-import ProductsContext from "./store/products-context";
 
 const App = () => {
     return (
         <div className="App">
             <Nav />
             <Hero />
-            <Main>
-                <Products />
-            </Main>
+            <Main></Main>
             <Footer />
         </div>
     );
