d197af0f716e9e55d8ad52beaf6d4d6521abf43d
alliyah95 <74038500+alliyah95@users.noreply.github.com>
2022-12-22 13:04:38 +0800

    Add animation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import OrderSummary from "./OrderSummary";
 
 const Cart = () => {
     return (
-        <div className="space-y-5 md:w-[80%] md:mx-auto md:flex md:justify-between md:space-y-0 md:space-x-5">
+        <div className="space-y-5 md:w-[80%] md:mx-auto md:flex md:justify-between md:space-y-0 md:space-x-5  animate__animated animate__fadeIn">
             <CartItems />
             <OrderSummary />
         </div>
