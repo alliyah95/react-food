7e3cdd084a8a501894e3933a9c3a892d46c82fd9
alliyah95 <74038500+alliyah95@users.noreply.github.com>
2022-12-22 12:37:41 +0800

    Update notification feature

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,12 @@ import React, { useContext } from "react";
 import Products from "./Products/Products";
 import Cart from "./Cart/Cart";
 import AppContext from "../store/app-context";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Main = () => {
     const appContext = useContext(AppContext);
+
+    const notify = (name) => toast(`${name} added to bag!`);
 
     return (
         <div className="container min-h-screen">
@@ -12,8 +15,20 @@ const Main = () => {
                 {appContext.view === "home" && "Delicious food for you!"}
                 {appContext.view === "cart" && "Your bag"}
             </h3>
-            {appContext.view === "home" && <Products />}
+            {appContext.view === "home" && <Products onAdd={notify} />}
             {appContext.view === "cart" && <Cart />}
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
     );
 };
