83d14ab10d338edd4e6b067bd654e1014c713131
alliyah95 <74038500+alliyah95@users.noreply.github.com>
2023-01-01 12:48:15 +0800

    fix: change the algorithm for checking cart length

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,30 @@
+import React, { useContext, useState, useEffect } from "react";
 import CartItems from "./CartItems";
 import OrderSummary from "./OrderSummary";
+import AppContext from "../../store/app-context";
 
 const Cart = () => {
+    const appContext = useContext(AppContext);
+    const [cartTotal, setCartTotal] = useState(0);
+
+    useEffect(() => {
+        setCartTotal(appContext.cartTotal);
+    }, [appContext.cartTotal]);
+
+    if (cartTotal > 0) {
+        return (
+            <div className="space-y-5 md:w-[80%] md:mx-auto md:flex md:justify-between md:space-y-0 md:space-x-5  animate__animated animate__fadeIn">
+                {<CartItems />}
+                {<OrderSummary />}
+            </div>
+        );
+    }
+
     return (
-        <div className="space-y-5 md:w-[80%] md:mx-auto md:flex md:justify-between md:space-y-0 md:space-x-5  animate__animated animate__fadeIn">
-            <CartItems />
-            <OrderSummary />
+        <div>
+            <p className="text-tertiary">
+                You have not selected anything yet...
+            </p>
         </div>
     );
 };
--- src/components/Cart/OrderSummary.js
+++ src/components/Cart/OrderSummary.js
@@ -14,39 +14,33 @@ const OrderSummary = () => {
         appContext.onChangeView("checkout");
     };
 
-    if (appContext.cart.length > 0 && total > 0) {
-        return (
-            <div className="flex flex-col self-start bg-gray md:w-2/5 lg:w-3/5 p-5">
-                <h3 className="text-xl font-bold mb-2">Order Summary</h3>
-                <p className="font-bold">Selected items:</p>
-                <ul className="space-y-2 mb-3">
-                    {appContext.cart.map((product) => {
-                        return (
-                            product.quantity > 0 && (
-                                <li key={product.id}>
-                                    <div>
-                                        <p>
-                                            {product.name} x {product.quantity}
-                                        </p>
-                                        <p className="text-tertiary">
-                                            $ {product.price * product.quantity}
-                                        </p>
-                                    </div>
-                                </li>
-                            )
-                        );
-                    })}
-                </ul>
+    return (
+        <div className="flex flex-col self-start bg-gray md:w-2/5 lg:w-3/5 p-5">
+            <h3 className="text-xl font-bold mb-2">Order Summary</h3>
+            <p className="font-bold">Selected items:</p>
+            <ul className="space-y-2 mb-3">
+                {appContext.cart.map((product) => {
+                    return (
+                        product.quantity > 0 && (
+                            <li key={product.id}>
+                                <div>
+                                    <p>
+                                        {product.name} x {product.quantity}
+                                    </p>
+                                    <p className="text-tertiary">
+                                        $ {product.price * product.quantity}
+                                    </p>
+                                </div>
+                            </li>
+                        )
+                    );
+                })}
+            </ul>
 
-                <h3 className="lg:mt-24 mb-3 text-xl font-bold">
-                    Total: ${total}
-                </h3>
-                <Button onclick={orderHandler} disabled={total <= 0}>
-                    Place order
-                </Button>
-            </div>
-        );
-    }
+            <h3 className="lg:mt-24 mb-3 text-xl font-bold">Total: ${total}</h3>
+            <Button onclick={orderHandler}>Place order</Button>
+        </div>
+    );
 };
 
 export default OrderSummary;
